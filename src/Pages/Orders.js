5ac11e3917d10b2628ecf309fd36b34c8b1b5960
Ritik2411 <73746458+Ritik2411@users.noreply.github.com>
2021-11-19 17:48:37 +0530

    Full Code

--- src/Pages/Orders.js
+++ src/Pages/Orders.js
@@ -4,15 +4,21 @@ import { useStateValue } from '../ContextApi/Stateprovider'
 import { db } from '../firebase'
 import Order from './Order'
 import './Orders.css'
-import Expire from '../Components/Expire'
+import ClipLoader from 'react-spinners/ClipLoader'
 
 function Orders() {
 
     const [{user},dispatch] = useStateValue()
+    const [loading,setLoading] = useState([false])
     const [orders,setOrders] = useState([])
     const history = useHistory()
-
+    
     useEffect(async()=>{
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 5000);
+
         if(user){
             await db.collection('users').doc(user?.uid).collection('orders').orderBy('created','desc').onSnapshot(snapshot=>(
                 setOrders(snapshot.docs.map((doc)=>({
@@ -34,6 +40,31 @@ function Orders() {
         <div className="orders">
             <h2>Orders</h2>
             {
+                loading?(
+                    <div style={{textAlign:'center'}}>
+                        <ClipLoader css="cliploader" size={30} color={"#123abc"} loading={loading}/>
+                    </div>):(
+                    (orders.length)>0?(
+                        <>
+                            <div className="orders_container">
+                            {
+                                
+                                orders.map(order=>{
+                                    return(
+                                        <Order order={order}/>
+                                    )
+                                })
+                            }
+                            </div>
+                        </>
+                        ):(
+                            <h2 style={{textAlign:"center"}}>
+                                No orders yet :(
+                            </h2>
+                        )
+                )
+            }
+            {/*{
                 (orders.length>0)?(
                     <div className="orders_container">
                         {
@@ -52,7 +83,7 @@ function Orders() {
                     </h2>
                     </>
                 )    
-            }
+            }*/}
             
             
  
